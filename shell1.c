c2fc4a543b2f80e317b75d316bc076e610116d62
KananKarim <kenankerimli7770@gmail.com>
2023-12-28 01:02:44 -0800

    betty is happy!

--- shell1.c
+++ shell1.c
@@ -1,32 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ * This function continuously reads lines from the standard input,
+ * executes them as system commands, and then prints a prompt for
+ *
+ * Return: EXIT_SUCCESS upon successful termination.
+ */
 
 int main(void)
 {
-  char *line = NULL;
-  size_t len = 0;
-  ssize_t read;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
 
-  while (1)
-  {
-      if (isatty(STDIN_FILENO))
-          printf("#cisfun$ ");
-    read = getline(&line, &len, stdin);
-    if (read == -1)
-    {
-	free(line);
-       if (!feof(stdin)) 
-            printf("\n");
-        exit(EXIT_SUCCESS);
-    }
-    else if (read > 1)
-    {
-        line[strcspn(line, "\n")] = 0;
-        system(line);
-    }
-  }
-  return (EXIT_SUCCESS);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("#cisfun$ ");
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			free(line);
+			if (!feof(stdin))
+				printf("\n");
+			exit(EXIT_SUCCESS);
+		}
+		else if (read > 1)
+		{
+			line[strcspn(line, "\n")] = 0;
+			system(line);
+		}
+	}
+	return (EXIT_SUCCESS);
 }
-
